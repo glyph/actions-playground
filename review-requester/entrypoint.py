fa7bf42b9eb4fd611e60b93a7547e441430af8c2
Glyph <glyph@twistedmatrix.com>
2019-09-15 01:26:10 -0700

    debug debug debug

--- review-requester/entrypoint.py
+++ review-requester/entrypoint.py
@@ -1,13 +1,16 @@
 #!/e/bin/python3.7
 
 from os import environ
+from json import load
+
 from twisted.internet import defer, task
 from gidgethub.treq import GitHubAPI
 
 async def main(reactor):
     with open("/github/workflow/event.json") as f:
-        print("EVENT JSON", f.read())
-    gh = GitHubAPI("", oauth_token=environ["GITHUB_TOKEN"])
-    print(await gh.getitem("/repos/glyph/actions-playground/issues/comments/1"))
+        event = load(f)
+        print("comment said", event['comment']['body'])
+    gh = GitHubAPI("glyph-actions-playground", oauth_token=environ["GITHUB_TOKEN"])
+    print(await gh.getitem("/repos/glyph/actions-playground/issues/comments/531545265"))
 
 task.react(lambda reactor, *argv: defer.ensureDeferred(main(reactor)))
