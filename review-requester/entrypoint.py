59c8a01000c9a158d53d27d4821a64a1c8f62ef9
Glyph <glyph@twistedmatrix.com>
2019-09-15 00:26:47 -0700

    let's go

---
+++ review-requester/entrypoint.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3.7
+
+from sys import stdin
+from os import environ
+from twisted.internet import defer, task
+from gidgethub.treq import GitHubAPI
+
+async def main(reactor):
+    print("STANDARD INPUT", stdin.read())
+    gh = GitHubAPI("", environ["INPUT_REPO_TOKEN"])
+    print(await gh.getitem("/repos/octocat/Hello-World/issues/comments/1"))
+
+task.react(lambda reactor, *argv: defer.ensureDeferred(main(reactor)))
