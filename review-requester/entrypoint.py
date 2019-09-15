f74aeb69d1ef60047208687bfdf8bc59e8fe7016
Glyph <glyph@twistedmatrix.com>
2019-09-15 01:18:13 -0700

    actually raed

--- review-requester/entrypoint.py
+++ review-requester/entrypoint.py
@@ -5,7 +5,7 @@ from twisted.internet import defer, task
 from gidgethub.treq import GitHubAPI
 
 async def main(reactor):
-    with open("workflow/event.json") as f:
+    with open("/github/workflow/event.json") as f:
         print("EVENT JSON", f.read())
     gh = GitHubAPI("", oauth_token=environ["GITHUB_TOKEN"])
     print(await gh.getitem("/repos/glyph/actions-playground/issues/comments/1"))
