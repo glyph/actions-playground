42c6318960b1105494e73f3986e62a27f2ed83f1
Glyph <glyph@twistedmatrix.com>
2019-09-15 01:36:26 -0700

    let's see how we do

--- review-requester/entrypoint.py
+++ review-requester/entrypoint.py
@@ -7,10 +7,24 @@ from twisted.internet import defer, task
 from gidgethub.treq import GitHubAPI
 
 async def main(reactor):
-    with open("/github/workflow/event.json") as f:
+    with open(environ['GITHUB_EVENT_PATH']) as f:
         event = load(f)
-        print("comment said", event['comment']['body'])
+    labels_url = event['issue']['labels_url']
+    command = event['comment']['body']
+    cmdwords = command.strip().split(" ")
+    if cmdwords[0] == '/label':
+        label = cmdwords[1]
+        operation = 'append'
+    elif cmdwords[0] == '/unlabel':
+        label = cmdwords[1]
+        operation = 'remove'
+    else:
+        print("nevermind")
+        return
     gh = GitHubAPI("glyph-actions-playground", oauth_token=environ["GITHUB_TOKEN"])
-    print(await gh.getitem("/repos/glyph/actions-playground/issues/comments/531545265"))
+    llist = (await gh.getitem(labels_url))['labels']
+    getattr(llist, operation)(label)
+    print("modified", llist)
+    await gh.post(labels_url, {"lables": llist})
 
 task.react(lambda reactor, *argv: defer.ensureDeferred(main(reactor)))
